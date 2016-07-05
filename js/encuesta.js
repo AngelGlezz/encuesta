3379504c1c035f697444e56c12f6eba0f322daf1
Vocho <vocho@underdog.gs>
2016-07-05 18:00:29 -0500

    secs
    ssdcs

--- js/encuesta.js
+++ js/encuesta.js
@@ -1,30 +1,25 @@
 $(document).ready(function(){
 	$("#si").click(function(){
-		$("#cover").fadeOut();
+		$("#cover").hide();
 		$("#uno").fadeIn();
 		$("#index_cont").show();
 		$("#fondo").css('background', 'url(images/backs/back2.jpg)');
 	});
+	$("#rifaste").click(click1);
+	$("#conquistaste").click(click2);
 	$(".pre_1").click(function(){
 		var div = '<div class="area_cont"><textarea class="area"></textarea><div class="boton" id="area1">ENVIAR</div></div>';
 		var id = $(this).attr('id');
 		switch (id) {
-			case 'rifaste':
-				$("#rifaste").addClass('uno');
-				$("#falto").hide();
-				$("#desastre").hide();
-				$("#ec1").append(div);
-				$(this).css('margin-left', '0px');
-			break;
 			case 'falto':
-				$("#falto").addClass('uno');
+				$("#falto").unbind('click');
 				$("#rifaste").hide();
 				$("#desastre").hide();
 				$("#ec1").append(div);
 				$(this).css('margin-left', '0px');
 			break;
 			case 'desastre':
-				$("#desastre").addClass('uno');
+				$("#desastre").unbind('click');
 				$("#falto").hide();
 				$("#rifaste").hide();
 				$("#ec1").append(div);
@@ -37,22 +32,15 @@ $(document).ready(function(){
 		var div = '<div class="area_cont"><textarea class="area"></textarea><div class="boton" id="area2">ENVIAR</div></div>';
 		var id = $(this).attr('id');
 		switch (id) {
-			case 'conquistaste':
-				$("#conquistaste").addClass('uno');
-				$("#maso").hide();
-				$("#suck").hide();
-				$("#ec2").append(div);
-				$(this).css('margin-left', '0px');
-			break;
 			case 'maso':
-				$("#maso").addClass('uno');
+				$("#maso").unbind('click');
 				$("#conquistaste").hide();
 				$("#suck").hide();
 				$("#ec2").append(div);
 				$(this).css('margin-left', '0px');
 			break;
 			case 'suck':
-				$("#suck").addClass('uno');
+				$("#suck").unbind('click');
 				$("#maso").hide();
 				$("#conquistaste").hide();
 				$("#ec2").append(div);
@@ -62,29 +50,24 @@ $(document).ready(function(){
 		$("#area2").click(click2);
 	});
 	$(".pre_3").click(function(){
-		$("#tres").fadeOut();
+		$("#tres").hide();
 		$("#final").fadeIn();
 		$("#index_cont").hide();
-<<<<<<< HEAD
 		$("#fondo").css('background', 'url(images/backs/back.jpg)');
-=======
->>>>>>> gh-pages
 	});
 	function click1(){
-		$("#uno").fadeOut();
+		$("#uno").hide();
 		$("#dos").fadeIn();
 		$("#i2 > img").attr('src', 'images/iconos/ic_active2.png');
 		$("#fondo").css('background', 'url(images/backs/back.jpg)');
+		$("#i2").css('background-color', '#fff');
 	};
 	function click2(){
-		$("#dos").fadeOut();
+		$("#dos").hide();
 		$("#tres").fadeIn();
-<<<<<<< HEAD
 		$("#i3 > img").attr('src', 'images/iconos/ic_active3.png');
 		$("#fondo").css('background', 'url(images/backs/back2.jpg)');
-=======
-		$("#i3").css('background', '#adf21f');
->>>>>>> gh-pages
+		$("#i3").css('background-color', '#fff');
 	};
 });
 
